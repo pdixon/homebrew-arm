233aa900e01de739697f3d619babb12116ccaf5d
Phillip DIXON <phil@dixon.gen.nz>
2013-01-15 21:55:20 +1300

    Builds for c but not c++

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -1,43 +1,25 @@
 require 'formula'
 
 class ArmNoneEabiGcc < Formula
-  homepage 'https://launchpad.net/gcc-arm-embedded'
-  url 'http://gcc.gnu.org/svn/gcc/branches/ARM/embedded-4_7-branch/', :using => :svn, :revision => '194305'
-  version '4.7-2012-q4'
+  homepage 'http://gcc.gnu.org'
+  url 'http://ftp.gnu.org/gnu/gcc/gcc-4.7.2/gcc-4.7.2.tar.bz2'
+  md5 'cc308a0891e778cfda7a151ab8a6e762'
 
+  depends_on 'arm-none-eabi-binutils'
   depends_on 'gmp'
   depends_on 'libmpc'
   depends_on 'mpfr'
-#  depends_on 'ppl'
-#  depends_on 'cloog'
 
   def install
     gmp = Formula.factory 'gmp'
     libmpc = Formula.factory 'libmpc'
     mpfr = Formula.factory 'mpfr'
-#    cloog = Formula.factory 'cloog'
-#    ppl = Formula.factory 'ppl'
 
-    # brew's build environment is in our way
-    ENV.delete 'CFLAGS'
-    ENV.delete 'CXXFLAGS'
-    ENV.delete 'AS'
-    ENV.delete 'LD'
-    ENV.delete 'NM'
-    ENV.delete 'RANLIB'
-    ENV.delete 'CPPFLAGS'
-    ENV.delete 'LDFLAGS'
-    #ENV.llvm
-    unless HOMEBREW_PREFIX.to_s == '/usr/local'
-      ENV['CPPFLAGS'] = "-I#{HOMEBREW_PREFIX}/include"
-      ENV['LDFLAGS'] = "-L#{HOMEBREW_PREFIX}/lib"
-    end
-
-    ENV.prepend 'PATH', bin, ':'
+    ENV.llvm
 
     args = [
             "--target=arm-none-eabi",
-            "--enable-languages=c,c++",
+            "--enable-languages=c",
             "--disable-decimal-float",
             "--disable-libffi",
             "--disable-libgomp",
@@ -59,9 +41,6 @@ class ArmNoneEabiGcc < Formula
             "--with-gmp=#{gmp.prefix}",
             "--with-mpfr=#{mpfr.prefix}",
             "--with-mpc=#{libmpc.prefix}",
-#            "--with-cloog=#{cloog.prefix}",
-#           "--with-ppl=#{ppl.prefix}",
-#            "--enable-cloog-backend=isl",
             # ...except the stuff in share...
             "--datarootdir=#{share}",
             # ...and the binaries...
@@ -80,6 +59,7 @@ class ArmNoneEabiGcc < Formula
       # binutils already has a libiberty.a. We remove ours, because
       # otherwise, the symlinking of the keg fails
       File.unlink "#{prefix}/lib/#{multios}/libiberty.a"
+      man7.rmtree
     end
   end
 end
